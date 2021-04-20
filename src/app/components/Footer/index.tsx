20f7f1635605e7658cf4274101aa850e58d06f70
hardi shah <hardishahhardi@gmail.com>
2021-04-20 21:36:07 +0530

    Updated file

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="dashboard-copyright">
+      &copy; 2021 Company.All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
--- src/app/pages/Login/index.tsx
+++ src/app/pages/Login/index.tsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import { FormGroup, Grid, TextField } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import { Logo } from "../../assets/images";
+import Footer from "../../components/Footer";
 
-function index() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Login = () => {
+  return (
+    <main className="main-content">
+      <Grid container>
+        <Grid item xl={5} md={6} className="login-left">
+          <div className="login-header">
+            <Link to="#">
+              <img src={Logo} alt="" className="img-responsive logo" />
+            </Link>
+          </div>
 
-export default index
+          <div className="login-form">
+            <h3 className="login-text">Login</h3>
+            <form>
+              <FormGroup>
+                <TextField id="uname" label="Username" />
+              </FormGroup>
+              <FormGroup>
+                <TextField id="Password" type="password" label="Password" />
+              </FormGroup>
+            </form>
+            <Grid container>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  type="button"
+                  variant="contained"
+                  color="primary"
+                  className="btn-login"
+                  disableRipple={true}
+                >
+                  Login
+                </Button>{" "}
+              </Grid>
+
+              <Grid item xs={12} sm={6} className="forgot-password">
+                <Link to="#">Forgot Password?</Link>
+              </Grid>
+            </Grid>
+            <div className="signup-account">
+              <span className="account">Don't have an account?</span>
+              <span>
+                <Link to="#" className="signup">
+                  Sign Up
+                </Link>
+              </span>
+            </div>
+          </div>
+          <Footer />
+        </Grid>
+
+        <Grid item md={6} xl={7}>
+          <div className="login-right"></div>
+        </Grid>
+      </Grid>
+    </main>
+  );
+};
+
+export default Login;
